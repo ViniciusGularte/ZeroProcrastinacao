3a1eed80181d2d859022c22dbcf7e21d6533d6e0
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2018-10-30 20:15:07 -0300

    subindo app

---
+++ filter.js
@@ -0,0 +1,7 @@
+console.log('Rodando aplicação');
+//Defina aqui os sites que você considera perder tempo
+const SitesDeProcrastinacao = [
+'www.facebook.com'
+];
+var parsedUrl = new URL(window.location.href);
+console.log(parsedUrl.href);
