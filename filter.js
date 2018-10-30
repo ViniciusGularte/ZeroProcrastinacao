db4c44e46cae13e4126fb96b653e741b04dac40a
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2018-10-30 20:34:24 -0300

    Adicionando metodos

--- filter.js
+++ filter.js
@@ -1,7 +1,15 @@
 console.log('Rodando aplicação');
-//Defina aqui os sites que você considera perder tempo
+//Defina aqui os sites que você considera procrastinar
 const SitesDeProcrastinacao = [
-'www.facebook.com'
+'https://www.facebook.com/','https:/www.youtube.com/','https:/www.linkedin.com/'
 ];
 var parsedUrl = new URL(window.location.href);
-console.log(parsedUrl.href);
+//Url atual
+const UrlAtual = parsedUrl.href;
+//verifica array de sites proibidos :V
+SitesDeProcrastinacao.forEach(function (sites) {
+  if(sites === UrlAtual){
+    alert('Você não pode acessar essa pagina, você nunca alcançara a maestria assim.. bora estudar :D');
+    history.back();
+  }
+});
