7a6f428deaec2579cc3fe15b5c1434620b6b0505
Vinicius Martin <viniciusgulartemartin@gmail.com>
2019-02-03 19:11:51 -0200

    add more files

--- filter.js
+++ filter.js
@@ -1,29 +1,26 @@
-//Defina aqui os sites que você considera procrastinar
+// Defina aqui os sites que você considera procrastinar
 const SitesDeProcrastinacao = [
-'www.facebook.com','www.youtube.com','www.linkedin.com'
+'www.facebook.com', 'www.youtube.com', 'www.linkedin.com'
 ];
-//Defina aqui sua frase motivacional ou deixe a api puxar a frase do dia
-const fraseMotivacionalPersonalizada = ""
-
+// Defina aqui sua frase motivacional ou deixe a api puxar a frase do dia
+const fraseMotivacionalPersonalizada = ''
 fetch('https://quotes.rest/qod.json')
-.then(response => response.json())
-.then(data => {
-  const fraseMotivacional = data.contents.quotes[0].quote
-  const fraseMotivacionalAutor = data.contents.quotes[0].author
-  //Url atual
-  const UrlAtual = window.location.href
-  //verifica array de sites proibidos :V
-  SitesDeProcrastinacao.forEach(function (sites) {
-    if(UrlAtual.includes(sites)){
-      if(fraseMotivacionalPersonalizada){
+  .then(response => response.json())
+  .then((data) => {
+    const fraseMotivacional = data.contents.quotes[0].quote
+    const fraseMotivacionalAutor = data.contents.quotes[0].author
+    // Url atual
+    const UrlAtual = window.location.href
+    // verifica array de sites proibidos :V
+    SitesDeProcrastinacao.forEach((sites) => {
+      if (UrlAtual.includes(sites)) {
+        if (fraseMotivacionalPersonalizada) {
           alert(fraseMotivacionalPersonalizada)
-      }
-      else{
+        } else {
           alert(`${fraseMotivacional} \n \n ${fraseMotivacionalAutor}`)
+        }
+        history.back()
       }
-
-      history.back()
-    }
-  });
+    });
 })
 .catch(error => console.error(error))
